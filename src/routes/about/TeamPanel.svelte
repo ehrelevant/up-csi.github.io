948e4b54d7dac83281b8778ed0cff390798e85c4
Ehren Castillo <ehrencastillo@gmail.com>
2024-08-15 00:59:53 +0800

    refactor(about): split team data from team panel component

--- src/routes/about/TeamPanel.svelte
+++ src/routes/about/TeamPanel.svelte
@@ -3,7 +3,7 @@
     import { Icon } from '@steeze-ui/svelte-icon';
     import { Link } from '@steeze-ui/heroicons';
     import TeamCard from './TeamCard.svelte';
-    import caleb from '$lib/people/caleb-bunye.webp';
+    import team from './team';
 
     const committees = [
         'Everyone',
@@ -14,99 +14,6 @@
         'Membership & Internals',
         'Branding & Creatives',
         'Engineering',
-    ];
-
-    const team = [
-        {
-            name: 'Caleb Bunye',
-            title: 'President',
-            committee: 'Executive Board',
-            img: caleb,
-            socials: {
-                github: 'https://github.com',
-                linkedin: 'https://linkedin.com',
-                instagram: 'https://instagram.com',
-                website: 'https://google.com',
-            },
-        },
-        {
-            name: 'Caleb Bunye 2',
-            title: 'President',
-            committee: 'Innovation',
-            img: caleb,
-            socials: {
-                github: 'https://github.com',
-                linkedin: 'https://linkedin.com',
-                instagram: 'https://instagram.com',
-                website: 'https://google.com',
-            },
-        },
-        {
-            name: 'Caleb Bunye 3',
-            title: 'President',
-            committee: 'Service',
-            img: caleb,
-            socials: {
-                github: 'https://github.com',
-                linkedin: 'https://linkedin.com',
-                website: 'https://google.com',
-            },
-        },
-        {
-            name: 'Caleb Bunye 4',
-            title: 'President',
-            committee: 'External Relations',
-            img: caleb,
-            socials: {
-                linkedin: 'https://linkedin.com',
-                instagram: 'https://instagram.com',
-                website: 'https://google.com',
-            },
-        },
-        {
-            name: 'Caleb Bunye 5',
-            title: 'President',
-            committee: 'Membership & Internals',
-            img: caleb,
-            socials: {
-                github: 'https://github.com',
-                linkedin: 'https://linkedin.com',
-                instagram: 'https://instagram.com',
-            },
-        },
-        {
-            name: 'Caleb Bunye 6',
-            title: 'President',
-            committee: 'Branding & Creatives',
-            img: caleb,
-            socials: {
-                github: 'https://github.com',
-                linkedin: 'https://linkedin.com',
-                instagram: 'https://instagram.com',
-            },
-        },
-        {
-            name: 'Caleb Bunye 7',
-            title: 'President',
-            committee: 'Engineering',
-            img: caleb,
-            socials: {
-                github: 'https://github.com',
-                instagram: 'https://instagram.com',
-                website: 'https://google.com',
-            },
-        },
-        {
-            name: 'Caleb Bunye 8',
-            title: 'President',
-            committee: 'Executive Board',
-            img: caleb,
-            socials: {
-                github: 'https://github.com',
-                linkedin: 'https://linkedin.com',
-                instagram: 'https://instagram.com',
-            },
-        },
     ];
 
     $: currentCommittee = 'Executive Board';
